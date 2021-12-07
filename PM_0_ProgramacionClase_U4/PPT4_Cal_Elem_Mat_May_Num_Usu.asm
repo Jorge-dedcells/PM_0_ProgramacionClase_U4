22ad02b3982c95c3db675b7c5f95fd675dfb3f50
Jorge-dedcells <78442071+Jorge-dedcells@users.noreply.github.com>
2021-12-06 19:26:47 -0600

    actualizar

---
+++ PM_0_ProgramacionClase_U4/PPT4_Cal_Elem_Mat_May_Num_Usu.asm
@@ -0,0 +1,63 @@
+TITLE NombrePrograma
+
+;DESCRPICIÓN 
+;Objetivo: Archivo de Ejemplo
+;
+; Autore(s):
+;         Apellido Nombre Integrante 1
+;         Apellido Nombre Integrante 2
+
+; Semestre: 8vo Semestre ISC 
+;FIN DESCRPICIÓN
+
+INCLUDE Irvine32.inc  
+INCLUDE MACROS.inc
+
+.data
+; Área de Declaración de Variables
+arreglo_p dword 10, 12, 15, 20
+		    dword  7,  5, 10, 12
+			dword  1,  2,  3,  4
+
+num dword ?
+
+.code
+
+	sumatr3 PROC 
+	
+	;escribe tu mensaje
+	ImpPantalla_N_Ln "Ingresa el numero: "
+	mov num, eax
+	mov ebx, 0
+	mov ecx, 11
+
+	Ciclo:
+		 mov eax, dword ptr[arreglo_p+ecx*4]
+		 
+         cmp eax, num
+		    jle NextPart
+		  
+		  inc ebx
+		  push eax
+
+		 NextPart:
+			 dec ecx
+			 cmp ecx, 0
+		 jge Ciclo
+
+
+   cicloprint:
+        
+		cmp ebx, 0
+		 je salir
+
+		 dec ebx
+		 pop eax
+		 ImpPantalla_N_Ln "Numeros mayores: "
+		 call crlf
+		 jmp cicloprint
+	
+	salir:
+	exit	
+	sumatr3 ENDP
+	END sumatr3
